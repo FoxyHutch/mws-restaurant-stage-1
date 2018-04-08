d4c82dc6e189434e5cb0accd224f4932e1bc87c6
Kester <kester.lotz@gmail.com>
2018-04-08 21:07:45 +0200

    style: Format Code an Delete Comments

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 const cacheName = 'v5';
 
-self.addEventListener('install', function(event){
+self.addEventListener('install', function (event) {
     console.log('SW: Service Worker installed');
     event.waitUntil(
-        caches.open(cacheName).then(function(cache){
+        caches.open(cacheName).then(function (cache) {
             console.log('SW: Caching Files');
             return cache.addAll([
                 '/skeleton',
@@ -20,14 +20,14 @@ self.addEventListener('install', function(event){
     )
 })
 
-self.addEventListener('activate', function(event){
+self.addEventListener('activate', function (event) {
     console.log('SW: Service Worker activated');
 
     //Delete old Cache if new Cacheversion is available
     event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(cacheNames.map(function(thisCacheName){
-                if(thisCacheName !== cacheName) {
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(cacheNames.map(function (thisCacheName) {
+                if (thisCacheName !== cacheName) {
                     console.log('SW: Deleting old Cache:', thisCacheName);
 
                     return caches.delete(thisCacheName);
@@ -38,58 +38,24 @@ self.addEventListener('activate', function(event){
     )
 })
 
-self.addEventListener('fetch', function(event){
-    console.log('SW: Service Worker is fetching for:', event.request.url);
+self.addEventListener('fetch', function (event) {
+    console.log('SW: Service Worker is fetching');
 
     event.respondWith(
-        caches.match(event.request).then(function(resp) {
+        caches.match(event.request).then(function (resp) {
 
 
-            return resp || fetch(event.request).then(function(response){
+            return resp || fetch(event.request).then(function (response) {
                 const responseClone = response.clone();
-                caches.open(cacheName).then(function(cache){
+                caches.open(cacheName).then(function (cache) {
                     cache.put(event.request, responseClone);
-                        
-            })
-            return response;
-        })
-        .catch(function(err){
-            console.log('SW: Error while fetching and Caching:', err);
-        })
 
-
-            /*
-            if( response ) {
-
-                console.log('SW: Corresponding Cache:', response);
-
+                })
                 return response;
-            }
-
-            const requestClone = event.request.clone();
-
-            fetch(requestClone)
-                .then(function(response){
-                    
-                    if(!response){
-                        console.log('SW: No Response from fetch')
-                        return response;
-                    }
-
-                    const responseClone = response.clone();
-
-                    caches.open(cacheName).then(function(cache){
-                    
-                        cache.put(event.request, responseClone);
-                        return response;
-                    
-                    });
-
-                }) 
-                .catch(function(err){
+            })
+                .catch(function (err) {
                     console.log('SW: Error while fetching and Caching:', err);
-                }); */
-
+                });
         })
     )
 })
